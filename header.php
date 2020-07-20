00bbd5852688302e36b813356ac28c566f95af6e
Roman <Roman290698@gmail.com>
2020-07-20 14:33:02 +0300

    final

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <style type="text/css">
+        .container {
+            text-align: center;
+            width: 400px;
+        }
+        #homePageContainer {
+            margin-top: 150px;
+        }
+        #containerLoggedInPage {
+            margin-top: 60px;
+        }
+        html {
+            background: url(bg.jpg) no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        body {
+            background: none;
+            color: #3E6B92;
+        }
+        #logInForm {
+            display:none;
+        }
+        .toggleForms {
+            font-weight: bold;
+        }
+        #diary {
+            width: 100%;
+            height: 100%;
+        }
+    </style>
+  </head>
+  <body>
